c177ff96473f3c43bab988750a1758f5a13cd637
Uday Bhanu <bhanusharm122@gmail.com>
2023-08-29 21:16:16 +0530

    api

--- index.js
+++ index.js
@@ -15,33 +15,11 @@ const client = new MongoClient(url, {
   useUnifiedTopology: true,
 });
 
-app.get("/", async (req, res) => {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-
-    const db = client.db(dbName);
-    const collection = db.collection("products");
-
-    //* Fetch all data from the collection
-    const cursor = collection.find();
-
-    //* Convert cursor to array of documents
-    const allData = await cursor.toArray();
-
-    console.log("Fetched all data:", allData);
-
-    res.json(allData); //* Send the fetched data as JSON response
-  } catch (err) {
-    console.error("Error fetching data:", err);
-    res.status(500).json({ error: "An error occurred" });
-  } finally {
-    client.close();
-    console.log("Connection closed");
-  }
+app.get("/getallproducts", async (req, res) => {
+  res.send("all products")
 });
 
-app.get("/getallproducts", async (req, res) => {
+app.get("/", async (req, res) => {
  res.send("Hello World!");
 });
 
