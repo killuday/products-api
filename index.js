b32a3db0bd90a96f84f3e20ec637eb9b60504aff
Uday Bhanu <bhanusharm122@gmail.com>
2023-08-29 21:38:29 +0530

    push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
 const cors = require('cors'); // Add this for CORS support
-const port = 3000;
+const port = 4000;
 
 // Enable CORS for all routes
 app.use(cors());
@@ -16,7 +16,29 @@ const client = new MongoClient(url, {
 });
 
 app.get("/getallproducts", async (req, res) => {
-  res.send("all products")
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db(dbName);
+    const collection = db.collection("products");
+
+    //* Fetch all data from the collection
+    const cursor = collection.find();
+
+    //* Convert cursor to array of documents
+    const allData = await cursor.toArray();
+
+    console.log("Fetched all data:", allData);
+
+    res.json(allData); //* Send the fetched data as JSON response
+  } catch (err) {
+    console.error("Error fetching data:", err);
+    res.status(500).json({ error: "An error occurred" });
+  } finally {
+    client.close();
+    console.log("Connection closed");
+  }
 });
 
 app.get("/", async (req, res) => {
