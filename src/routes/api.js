8019008b7bcad7dba35bfc0d10ced2b8d093c551
Uday Bhanu <bhanusharm122@gmail.com>
2023-10-29 20:25:08 +0530

    Login And Signup API

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,74 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt=require("bcrypt");
 const { ObjectId } = require("mongodb");
 const { client, dbName } = require("../db");
+const jwt = require('jsonwebtoken');
 
-const Product = require("../models/productModel");
 
+// const Product = require("../models/productModel");
+// const User=require("../models/userAuthModel");
+
+//login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection("users");
+  // Find the user by their email
+  const user = await collection.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  // Compare the provided password with the stored hashed password
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (passwordMatch) {
+    // Passwords match; generate a JWT token and send it to the client
+    const token = jwt.sign({ userId: user._id }, "your-secret-key", { expiresIn: "1h" }); // Use a strong, unique secret key.
+
+    res.status(200).json({ message: "Login successful", token });
+  } else {
+    // Passwords don't match
+    res.status(401).json({ error: "Invalid credentials" });
+  }
+});
+
+// Signup endpoint
+router.post("/signup", async (req, res) => {
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection("users"); // Use "users" collection
+
+    const { username, email, password } = req.body;
+
+    // Check if the provided email already exists in the "users" collection
+    const existingUser = await collection.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User with this email already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user document
+    const newUser = {
+      username,
+      email,
+      password: hashedPassword,
+      registeredOn: new Date(),
+    };
+
+    // Insert the new user into the "users" collection
+    await collection.insertOne(newUser);
+
+    res.status(201).json({ message: "User registered successfully", data: newUser });
+  } catch (err) {
+    console.error("Error registering user:", err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
 
 // For 'deleteproduct' route
 router.delete('/deleteproduct/:id', async (req, res) => {
@@ -113,7 +177,7 @@ router.get("/getallproducts", async (req, res) => {
     const cursor = collection.find();
     const allData = await cursor.toArray();
 
-    console.log("Fetched all data:", allData);
+    // console.log("Fetched all data:", allData);
 
     res.json(allData);
   } catch (err) {
